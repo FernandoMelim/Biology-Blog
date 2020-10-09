c128a4eae7b4f8c3c8246fdd27590d8bf6a2a36e
Fernando Hottum <Fernando.Hottum@thomsonreuters.com>
2020-10-09 02:49:24 -0300

    First changes

--- Biology-Blog/Controllers/WeatherForecastController.cs
+++ Biology-Blog/Controllers/WeatherForecastController.cs
@@ -24,7 +24,22 @@ namespace Biology_Blog.Controllers
         }
 
         [HttpGet]
+        [Route("Get")]
         public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetWrongData")]
+        public IEnumerable<WeatherForecast> GetWrongData()
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
